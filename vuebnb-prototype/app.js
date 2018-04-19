cb5b2ae97419ed83dad5a79ed05566f0f8d7868b
iamlili <liliemails@gmail.com>
2018-04-19 15:58:54 +1000

    Chapter 2 done

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,68 @@
+
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: sample.title,
+        address: sample.address,
+        about: sample.about,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        },
+        amenities: sample.amenities,
+        prices: sample.prices,
+        contracted: true,
+        modalOpen: false
+    },
+    
+    /*  We want to prevent bkg scrolling when the modal is open, so we need to add overflow hidden on the body tag
+        Vue can't update the body tag because it only has dominion over the element it's mounted on eg '#app'
+        Therefore we use a 'watcher' to update the body tag with regular js when the variable 'modalOpen' changes
+        (Watchers allow us to write custom logic that hooks into reactive data changes) */
+    watch: { 
+        modalOpen: function() {
+            var className = 'modal-open';
+            if (this.modalOpen) {
+                document.body.classList.add(className);
+            } else {
+                document.body.classList.remove(className);
+            }
+        }  
+    },
+
+    /*  We want to close the nodal by Pressing the escape key.
+        Vue has key event listeners like eg. <input v-on:keyup.esc="handler">
+        However in this modal case, on what element would we add the directive? Unfortunately, unless an input is focused on, key events are dispatched from the body element (which is outside of Vue's jurisdiction)
+        Therefore (like the modalOpen watcher above we have to resort to the Web API again)
+        But instead of using a Watcher, we hook into one of vue's Lifecycle Hooks (ie 'created' - which is run after the data is proxied) ie our eventlisteners will be created during Vue's initialisation
+        (this way we are guaranteed that the modalOpen variable is defined - we could have added the event listener outside of our Vue app config, but a user might press escape before Vue has proxied the data resulting in app.modalOpen being undefined )
+         */
+    created: function() {
+        document.addEventListener('keyup', this.escapeKeyListener);
+    },
+
+    /*  Using the 'Destroy' lifecycle hook, we use removeEventListener to get rid of the listener when the Vue instance is torn down, to avoid any memory leaks */
+    destroyed: function () {
+        document.removeEventListener('keyup', this.escapeKeyListener);
+    },
+
+    methods: {
+        /* We had this function originally outside of Vue, but the advantage to Vue methods is that they are passed the Vue instance as context and therefore have easy access to your other properties and methods. */
+        escapeKeyListener: function(evt) {
+            if (evt.keyCode === 27 && this.modalOpen) {
+                this.modalOpen = false;
+            }
+        }
+    }
+});
+
+/* We've turned this function into a Vue instance method above, and call it using this.escapeKeyListener within the instance */
+/*
+function escapeKeyListener(evt) {
+    if (evt.keyCode === 27 && app.modalOpen) {
+      app.modalOpen = false;
+    }
+}
+ */
