1b9f4e51497adea9c68d40397b5b4a1fb25a305d
iamlili <liliemails@gmail.com>
2018-05-11 16:49:18 +1000

    Chapter 7: Done. Homepage Slider.
    - reuse carousel-control component used in our listing page modal image slider
    - use css to translate the images horizontally when the change-image event is triggered from the carousel control component

--- vuebnb/resources/assets/components/HomePage.vue
+++ vuebnb/resources/assets/components/HomePage.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="home-container">
-        <div v-for="(group, country) in listing_groups" class="listing-summary-group">
-            <h1>Places in {{country}}</h1>
-            <div class="listing-summaries">
-                <listing-summary v-for="(listing) in group"
-                    :key="listing.id"
-                    :listing="listing">
-                </listing-summary>
-            </div>
-        </div>
+        <listing-summary-group class="listing-summary-group"
+            v-for="(group, country) in listing_groups"
+            :key = "country"
+            :country = "country"
+            :listings = "group">
+        </listing-summary-group>
     </div>
 </template>
 <script>
     import { groupByCountry } from '../js/helpers'; // we import a helper function to group the listing by country
-    import ListingSummary from './ListingSummary.vue';
+    import ListingSummaryGroup from './ListingSummaryGroup.vue';
 
     import axios from 'axios'; //for performing ajax. Already included in Laravel's default frontend code. eg axios.get('my-url'); To get the response chain a 'then' callback
     import routeMixin from '../js/route-mixin'; // mixins store functionality common between components. A mixin is an object like a component object. To use it declare it in an array assigned to the component config property 'mixin'. When the component in instantiated, any mixins will be merged with what else is in the component
@@ -26,7 +23,7 @@
         data() {
             return { listing_groups: [] } // now that we've moved serverData into the navigation guard and we are getting listing_groups after the component is created, we must initialise it as an empty array, as Vue must know the names of all reactive data properties
         },
-        components: { ListingSummary },
+        components: { ListingSummaryGroup },
         mixins: [ routeMixin ],
         methods: {
             assignData( { listings } ){ // this method is used from our routeMixin
@@ -37,20 +34,6 @@
     }
 </script>
 <style>
-    .listing-summary-group {
-        padding-bottom: 20px;
-    }
-    .listing-summaries {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        overflow: hidden;
-    }
-    .listing-summaries > .listing-summary {
-        margin-right: 15px;
-    }
-    .listing-summaries > .listing-summary:last-child {
-        margin-right: 0;
-    }
+  
 </style>
 
