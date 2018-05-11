b837a6eeaa44f34855749c6d9fe3d0a4eeeaebf2
iamlili <liliemails@gmail.com>
2018-05-11 15:22:34 +1000

    Chapter 7: Scroll behaviour between routes

--- vuebnb/resources/assets/js/router.js
+++ vuebnb/resources/assets/js/router.js
@@ -11,5 +11,9 @@ export default new VueRouter ({
     routes: [
         { path: '/', component: HomePage, name: 'home' },
         { path: '/listing/:listing', component: ListingPage, name: 'listing' }
-    ]
+    ],
+    /* Before adding these lines, if we changed routes, the scroll position would remain the same eg.if you were at the bottom of a page, clicked to go to another route, you would still be at the bottom of the screen  */
+    scrollBehavior (to, from, savedPosition) {
+        return { x: 0, y: 0 } // we're keeping it simple, and now will always be at the top of the page when changing routes
+    }
 });
